e22fda12a0f2628ad127698b144e0195b3eb28b3
FrancescoM95 <mon95fran@gmail.com>
2024-01-25 11:34:53 +0100

    milestone 2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 console.log('Vue OK', Vue);
 
+const { user, contacts } = data;
+
 const { createApp } = Vue;
+
 const app = createApp({
     name: 'Boolzapp',
     data: () => ({
-        data,
+        user,
+        contacts,
+        activeId: Math.min(...contacts.map(contact => contact.id)),
     }),
     computed: {
-
+        currentContact() {
+            return this.contacts.find(contact => contact.id === this.activeId);
+        }
     },
     methods: {
-
+        setActiveId(id) {
+            this.activeId = id;
+        }
     }
 });
 
