1762f95530b00466ea763aa5cc02e9d3a2a0bb6b
FrancescoM95 <mon95fran@gmail.com>
2024-01-26 14:52:11 +0100

    general review

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const { user, contacts } = data;
 const { createApp } = Vue;
 
 const app = createApp({
+    //# DATA
     name: 'Boolzapp',
     data: () => ({
         user,
@@ -12,32 +13,37 @@ const app = createApp({
         activeId: Math.min(...contacts.map(contact => contact.id)),
         currentInput: '',
         searchText: '',
+        lastLog: `Ultimo accesso oggi alle ${new Date().toLocaleTimeString()}`
     }),
+
+    //# COMPUTED
     computed: {
-        currentContact() {
+        currentContact() {  //* Funzione che restituisce l'id del contatto attivo
             return this.contacts.find(contact => contact.id === this.activeId);
         },
-        filteredContacts() {
+        filteredContacts() {  //* Funzione che restituisce un array filtrato in base all'input dell'utente
             const textToSearch = this.searchText.toLowerCase();
             return this.contacts.filter(contact => contact.name.toLowerCase().includes(textToSearch));
         }
     },
+
+    //# METHODS
     methods: {
-        setActiveId(id) {
+        setActiveId(id) {  //* Funzione che imposta come id attivo l'id del contatto ricevuto come parametro
             this.activeId = id;
         },
-        getLastMessage(contact) {
+        getLastMessage(contact) {  //* Funzione che, se presente, restituisce l'ultimo messaggio con il contatto ricevuto come parametro 
             if (contact.messages.length) {
                 const lastMessage = contact.messages[contact.messages.length - 1];
                 return lastMessage.text;
             } return ''
 
         },
-        getLastMessageDate(contact) {
+        getLastMessageDate(contact) {   //* Funzione che restituisce la data dell'ultimo messaggio
             const lastMessage = contact.messages[contact.messages.length - 1];
             return lastMessage.date;
         },
-        sendMessage() {
+        sendMessage() {    //* Funzione che aggiunge un nuovo messaggio inviato alla chat
             if (this.currentInput.trim() !== '') {
                 const newMessage = {
                     id: this.currentContact.messages.length + 1,
@@ -47,8 +53,9 @@ const app = createApp({
                 };
                 this.currentContact.messages.push(newMessage);
                 this.currentInput = '';
+                this.lastLog = 'Sta scrivendo...';
 
-                setTimeout(() => {
+                setTimeout(() => {  //* Funzione che simula risposta al messaggio
                     const responseMessage = {
                         id: this.currentContact.messages.length + 1,
                         date: new Date().toLocaleString(),
@@ -56,10 +63,11 @@ const app = createApp({
                         status: 'received',
                     };
                     this.currentContact.messages.push(responseMessage);
+                    this.lastLog = `Ultimo accesso oggi alle ${new Date().toLocaleTimeString()}`;
                 }, 1000);
             }
         },
-        deleteMessage(messageId) {
+        deleteMessage(messageId) {  //* Funzione per eliminare il messaggio dalla chat
             if (this.currentContact.messages.length > 1) {
                 this.currentContact.messages = this.currentContact.messages.filter(message => message.id !== messageId);
             } else {
